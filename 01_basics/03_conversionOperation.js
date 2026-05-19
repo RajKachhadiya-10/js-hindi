8185a2ec614139dd7530b69d70f06ee27c1ff71c
RajKachhadiya-10 <kachhadiyaraj76@gmail.com>
2026-05-19 18:10:08 +0000

    conv

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = 33
+console.log(typeof score)
+// also we can write it as 
+console.log(typeof(score))
+
+score = "33"
+console.log(typeof score)
+
+score = "33"
+let convToNum=Number(score)
+console.log(convToNum)
+
+score = "33abc"
+convToNum = Number(score)
+console.log(convToNum)
+
+score = null
+convToNum = Number(score)
+console.log(convToNum)
+
+score = undefined
+convToNum = Number(score)
+console.log(convToNum)
