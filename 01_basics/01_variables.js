9ddfae1585d685066b2cf3bcf895d131e1e77604
RajKachhadiya-10 <kachhadiyaraj76@gmail.com>
2026-05-19 14:54:59 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId=144553
+let accountEmail="kachhadiyaraj76@gmail.com"
+var accountPassword="12345"
+accountCity="Junagadh"
+console.table([accountEmail, accountId, accountPassword, accountCity])
---
+++ 01_basics/02_variables.js
@@ -0,0 +1,15 @@
+"use strict"; 
+//alert(3+3);
+console.log(3+3) 
+console.log("raj")
+console.log(typeof "Raj")
+console.log(typeof null)
+console.log(typeof undefined)
+
+/*variables
+number,Bigint
+string
+boolean
+null=> standlone value
+symbol => unique
+*/
