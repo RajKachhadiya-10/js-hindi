847747966b253632bfbed9606ba7214e851f99d7
RajKachhadiya-10 <kachhadiyaraj76@gmail.com>
2026-05-22 11:03:21 +0000

    array2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,36 @@
+let array1 = [1, 3, 5, 7];
+let array2 = [2, 4, 6, 8];
+
+// array1[9]=17;
+// console.log(Object.keys(array1));
+// console.log(array1.length);
+
+array1.length = 10
+console.log(array1);
+
+// console.log(array1["0"]);
+
+// array1.push(array2);
+// console.log((array1));
+
+// let array3 = array1.concat(array2);
+// console.log((array3));
+
+// let array4 = [...array1,...array2];
+// console.log(array4);
+
+// let complexArray = [1, 2, [2, 3, 4], 5, 6, 7, [8, [9, 10]]];
+// let easyArray = complexArray.flat(Infinity);
+// console.log((easyArray));
+
+
+// console.log(Array.isArray("Raj"));
+// console.log(Array.from("Raj"));
+// console.log(Array.from({name: "Raj"}));
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+// let score4 = 400;
+// console.log(Array.of(score1, score2, score3, score4));
+
