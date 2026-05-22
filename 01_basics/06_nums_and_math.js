1cd46a2bebdcd415cad53319db90a0056ef9a266
RajKachhadiya-10 <kachhadiyaraj76@gmail.com>
2026-05-22 08:51:16 +0000

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,19 @@
+ const score = 100
+ const balance = new Number(100)
+ console.log(score);
+ console.log(balance);
+ console.log(balance.toString());
+ console.log(balance.toFixed(2));
+
+ console.log(1.2e+2===12e+1);
+ 
+ const hundered = 10000000
+ console.log(hundered.toLocaleString('en-IN'));  
+
+console.log(Math.random());
+console.log(Math.random() * 10 + 1); // it give us value between 1 and 11 
+console.log(Math.floor(Math.random() * 10 + 1)); // it give us value between 1 and 10 
+
+let max,min;
+console.log((Math.random()*(max-min))+min);
+
