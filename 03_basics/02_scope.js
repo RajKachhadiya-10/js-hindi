79a07bced2c27f84da3866e4620006e003a68c90
RajKachhadiya-10 <kachhadiyaraj76@gmail.com>
2026-05-24 06:54:42 +0000

    scopes

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,34 @@
+if(true){
+    let a = 10;
+    const b = 20;
+    var c  = 30;
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+   // console.log(website);
+    two();
+}
+one();
+
+console.log(addOne(5))
+
+function addOne(num){
+    return num+1;
+}
+ 
+
+// console.log(addTwo(5));
+const addTwo = function(num){
+    return num+2;
+}
+console.log(addTwo(5));
